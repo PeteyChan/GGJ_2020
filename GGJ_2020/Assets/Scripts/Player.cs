abb8579a91a5978865d77ce2395b27b58bd7333b
Sean Harrison <seanharrs97@hotmail.com>
2020-02-01 14:29:20 +1000

    fix merge conflict in player script

--- GGJ_2020/Assets/Scripts/Player.cs
+++ GGJ_2020/Assets/Scripts/Player.cs
@@ -8,11 +8,7 @@
 
 public class Player : MonoBehaviour
 {
-<<<<<<< HEAD
     public GamePad GamePad = new GamePad(0);
-=======
-    public GamePad GamePad => new GamePad(0);
->>>>>>> a1e2f34855704a56695490854cf19c137bc9c044
 
     public ShipPart HeldPart { get; set; }
 
