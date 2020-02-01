09e60b35620bc973bbf5a0743773a3feef75e387
= <=>
2020-02-01 12:11:45 +1000

    Initial implementation of picking up and droping ship parts

--- GGJ_2020/Assets/Scripts/States/PickupState.cs
+++ GGJ_2020/Assets/Scripts/States/PickupState.cs
@@ -7,13 +7,30 @@ public class PickupState : State
 {
     [SerializeField] private float _timeToPickupItem;
 
+    private ItemCarryController _controller;
+
+    private void Awake()
+    {
+        _controller = gameObject.Find<ItemCarryController>();
+    }
+
+    protected override void OnEnter()
+    {
+        _controller.BeginPickupPart();
+    }
+
     protected override IState OnUpdate(float deltaTime, float stateTime)
     {
         if (stateTime > _timeToPickupItem)
         {
-            return GetComponent<CarryIdleState>();
+            return gameObject.Find<CarryIdleState>();
         }
 
         return this;
+    }
+
+    protected override void OnExit()
+    {
+        _controller.EndPickupPart();
     }
 }
