e36c0611ccf9d1e7f1126737bd13c52dabb3986c
PeteysLaptop <peterlevonpera@gmail.com>
2020-02-02 15:25:18 +1000

    I added some dodgy assets to the title

---
+++ GGJ_2020/Assets/ColorTOWinner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTOWinner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var mat = GameSettings.WinningTeam == GameSettings.Team.Blue ? Resources.Load<Material>("Blue") : Resources.Load<Material>("Red");
+        foreach (var renderer in GetComponentsInChildren<Renderer>())
+            renderer.sharedMaterial = mat;
+    }
+    
+}
